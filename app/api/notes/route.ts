386df8bd7e4edaf2aff2aed9c90cb9341287dcc2
eugeniocacahiombo <eugeniocachiombo@gmail.com>
2026-07-22 13:37:02 +0100

    perf: mensagem das validações  com zod num service separado

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,5 +1,18 @@
 import { list, create } from "@/app/repositories/note.repository";
 import { NextResponse } from "next/server";
+import { z } from "zod";
+import {errorZodMessages} from '@/app/services/zod.services'
+
+
+const noteValidator = z.object({
+  word: z
+    .string()
+    .min(1, "A palavra é obrigatória."),
+
+  mean: z
+    .string()
+    .min(1, "O significado é obrigatório."),
+});
 
 export async function GET() {
   const data = await list();
@@ -8,17 +21,12 @@ export async function GET() {
 
 export async function POST(request: Request) {
   try {
-
     const body = await request.json();
-    const note = await create({
-      word: body.word,
-      mean: body.mean,
-      description: body.description,
-    });
-
-    return NextResponse.json(note, {status: 201});
+    const data = noteValidator.parse(body); // Validar campos
+    const note = await create(data);
+    return NextResponse.json(note, { status: 201 });
   } catch (error) {
-    console.log(error);
-    return NextResponse.json({message: "Erro ao criar palavra", status: 500} );
+    if (error instanceof z.ZodError) { return errorZodMessages(error); }
+    return NextResponse.json({ message: "Erro ao criar palavra", status: 500 });
   }
 }
---
+++ app/services/zod.services.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export const errorZodMessages = (error: any) => {
+    return NextResponse.json(
+        {
+            errors: error.issues.map(e => ({
+                [e.path.join("")]: e.message
+            }))
+        },
+        { status: 422 }
+    );
+}
