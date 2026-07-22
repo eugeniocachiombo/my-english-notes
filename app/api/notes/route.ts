b89c365e2c803b4a9e5263e0b10ed684f7039bbf
eugeniocacahiombo <eugeniocachiombo@gmail.com>
2026-07-22 10:59:18 +0100

    perf: cadastrando palavras

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,7 +1,24 @@
-import {list} from '@/app/repositories/note.repository';
-import { NextResponse } from 'next/server.js';
+import { list, create } from "@/app/repositories/note.repository";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const data = await list();
   return NextResponse.json(data);
+}
+
+export async function POST(request: Request) {
+
+  try {
+
+    const body = await request.json();
+    const note = await create({
+      word: body.word,
+      mean: body.mean,
+      description: body.description,
+    });
+
+    return NextResponse.json(note, {status: 201});
+  } catch (error) {
+    return NextResponse.json({message: "Erro ao criar palavra", status: 500} );
+  }
 }
--- app/repositories/note.repository.ts
+++ app/repositories/note.repository.ts
@@ -3,3 +3,7 @@ import prisma from "../lib/prisma";
 export async function list() {
   return prisma.note.findMany();
 }
+
+export async function create(data) {
+  return prisma.note.create({data});
+}
